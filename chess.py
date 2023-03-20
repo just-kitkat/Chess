badaa64d8af04c997733fc8adc06b548dfe61bd7
kitkat3141 <121209669+kitkat3141@users.noreply.github.com>
2023-03-20 18:47:22 +0800

    removed castling into check from bugs list. (no such bug!)

--- chess.py
+++ chess.py
@@ -607,7 +607,6 @@ To-Do:
 
 Bugs:
 - Castling:
-    - Check if player can castle into check (low chance)
     - Fix player being able to castle out of check
     - Fix player being able to castle the rook into check
 """
