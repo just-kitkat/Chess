56c4e1ba7bc5a4680bb1faad91daaff8dc8f8b5b
kitkat3141 <121209669+kitkat3141@users.noreply.github.com>
2023-04-17 16:31:56 +0800

    Added todo: Make board centered

--- chess.py
+++ chess.py
@@ -714,6 +714,7 @@ To-Do:
 - Add stalemate. (game currently just does not allow the player to move)
 - Make move indicator smaller
 - If piece can be taken, change move indicator shape to a grey square with transparent circle in the center
+- Make board centered on screen
 
 Verified Bugs:
 - 
